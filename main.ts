bde31c13267158bce335fd7a7307ce6834fa2fc7
pei hsuan shih <91247011+318506@users.noreply.github.com>
2021-11-11 16:46:16 +0800

    MakeCode 專案的初始檔案

---
+++ main.ts
@@ -0,0 +1,114 @@
+input.onButtonPressed(Button.A, function () {
+    basic.clearScreen()
+    listt = [
+    0,
+    1,
+    0,
+    1,
+    0
+    ]
+    for (let index = 0; index <= 4; index++) {
+        if (listt[index] == 1) {
+            led.plot(index, 0)
+        } else {
+            led.unplot(index, 0)
+        }
+    }
+})
+input.onButtonPressed(Button.B, function () {
+    basic.clearScreen()
+    listtt = [
+    [
+    randint(0, 255),
+    randint(0, 255),
+    randint(0, 255),
+    randint(0, 255),
+    randint(0, 255)
+    ],
+    [
+    randint(0, 255),
+    randint(0, 255),
+    randint(0, 255),
+    randint(0, 255),
+    randint(0, 255)
+    ],
+    [
+    randint(0, 255),
+    randint(0, 255),
+    randint(0, 1),
+    randint(0, 1),
+    randint(0, 1)
+    ],
+    [
+    randint(0, 255),
+    randint(0, 255),
+    randint(0, 255),
+    randint(0, 255),
+    randint(0, 255)
+    ],
+    [
+    randint(0, 255),
+    randint(0, 255),
+    randint(0, 255),
+    randint(0, 255),
+    randint(0, 255)
+    ]
+    ]
+    for (let y = 0; y <= 4; y++) {
+        for (let x = 0; x <= 4; x++) {
+            led.plotBrightness(x, y, list[y][x])
+        }
+    }
+})
+let listtt: number[][] = []
+let listt: number[] = []
+let list: number[][] = []
+list = [
+[
+randint(0, 1),
+randint(0, 1),
+randint(0, 1),
+randint(0, 1),
+randint(0, 1)
+],
+[
+randint(0, 1),
+randint(0, 1),
+randint(0, 1),
+randint(0, 1),
+randint(0, 1)
+],
+[
+randint(0, 1),
+randint(0, 1),
+randint(0, 1),
+randint(0, 1),
+randint(0, 1)
+],
+[
+randint(0, 255),
+randint(0, 255),
+randint(0, 255),
+randint(0, 255),
+randint(0, 255)
+],
+[
+randint(0, 1),
+randint(0, 1),
+randint(0, 1),
+randint(0, 1),
+randint(0, 1)
+]
+]
+for (let y = 0; y <= 4; y++) {
+    for (let x = 0; x <= 4; x++) {
+        if (list[y][x] == 1) {
+            led.plot(x, y)
+        } else {
+            led.unplot(x, y)
+        }
+    }
+}
+basic.forever(function () {
+	
+})
